463e06b307c03bab67e491ee6d0a7be5607309c6
johnsila <60229418+johnsila@users.noreply.github.com>
2020-03-05 19:56:11 +0300

    Add files via upload

---
+++ PythonLoops.py
@@ -0,0 +1,12 @@
+#while loop
+count=0
+while(count<10):
+    count=count ++1
+    print("while loop") 
+
+
+#for loop
+students=['john','mercy','kamau']
+print(students)
+for student in students:
+    print(student)
